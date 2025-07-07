bedecac05d0f9639625e107bce6e38096ddd2001
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-07 21:17:40 +0100

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Define two variables
+
+number1 = 10
+number2 = 5
+
+# Perform addition
+addition_result = number1 + number2
+# Perform subtraction
+subtraction_result = number1 - number2
+# Perform multiplication
+multiplication_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Subtraction of {number1} and {number2} is {subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")

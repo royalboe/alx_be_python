9e4a5a012d963da0859345ea4dd33d2c0463fbb5
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-07 21:33:39 +0100

    removed constant

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -3,11 +3,9 @@ Python script that converts a specific number of hours into seconds.
 This task reinforces the concept of arithmetic operations within a practical context.
 """
 
-SECONDS = 3600  # Number of seconds in one hour
-
 hours = 2
 # Convert hours to seconds
-seconds = hours * SECONDS
+seconds = hours * 3600
 
 # Print the result
 print(f"{hours} hour(s) is {seconds} seconds.")

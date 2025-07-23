5156b7b40e087d2aa367f8e4ec070ccefbcd5b37
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-23 20:57:09 +0100

    changed wordings

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,10 +13,10 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
         elif choice == '2':
-            item = input("Enter item to remove: ")
+            item = input("Enter the item to remove: ")
             shopping_list.remove(item) if item in shopping_list else print(f"Item '{item}' not found in the list.")
         elif choice == '3':
             print("Shopping List:")

6a13ed80e05f0e48b88a8ce9c0e92e87c8598de8
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-23 20:45:03 +0100

    removed type hinting

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 
-def perform_operation(num1: float, num2: float, operation: str) -> float:
+def perform_operation(num1, num2, operation) -> float:
     """
     Perform arithmetic operations on two numbers.
 

15ef6fc6b68b4a2e22484035ed24257a37b89470
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-23 20:50:03 +0100

    changed to if else

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -15,18 +15,32 @@ def perform_operation(num1, num2, operation):
     ValueError: If an unsupported operation is provided.
     ZeroDivisionError: If division by zero is attempted.
     """
-    
-    match operation.lower():
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 == 0:
-                raise ZeroDivisionError("Cannot divide by zero.")
-            return num1 / num2
-        case _ as unsupported:
-            raise ValueError(f"Unsupported operation: {operation}")
+    operation = operation.strip().lower()  # Normalize the operation string
+    operations = ['add', 'subtract', 'multiply', 'divide']
+    if operation == operations[0]:
+        return num1 + num2
+    elif operation == operations[1]:
+        return num1 - num2
+    elif operation == operations[2]:
+        return num1 * num2
+    elif operation == operations[3]:
+        if num2 == 0:
+            raise ZeroDivisionError("Cannot divide by zero.")
+        return num1 / num2
+    else:
+        raise ValueError(f"Unsupported operation: {operation}")
+
+    # match operation:
+    #     case operations[0]:
+    #         return num1 + num2
+    #     case operations[1]:
+    #         return num1 - num2
+    #     case operations[2]:
+    #         return num1 * num2
+    #     case operations[3]:
+    #         if num2 == 0:
+    #             raise ZeroDivisionError("Cannot divide by zero.")
+    #         return num1 / num2
+    #     case _ as unsupported:
+    #         raise ValueError(f"Unsupported operation: {operation}")
           

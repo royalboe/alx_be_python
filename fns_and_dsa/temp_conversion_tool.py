4faceb0a3c5e493ade856e22ea54244869d1bc05
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-07-23 21:34:35 +0100

    functionality added

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,22 +1,3 @@
-"""
-Create a Python script named temp_conversion_tool.py. This script will define functions to convert temperatures between Celsius and Fahrenheit, demonstrating the use of global variables to store conversion factors that are accessible within functions.
-
-Requirements:
-Define Global Conversion Factors:
-
-At the top of your script, define two global variables FAHRENHEIT_TO_CELSIUS_FACTOR and CELSIUS_TO_FAHRENHEIT_FACTOR to store the conversion factors (e.g., (5/9) for F to C and (9/5) for C to F, respectively).
-Implement Conversion Functions:
-
-Write a function convert_to_celsius(fahrenheit) that takes a temperature in Fahrenheit and returns the temperature converted to Celsius.
-Write a function convert_to_fahrenheit(celsius) that takes a temperature in Celsius and returns the temperature converted to Fahrenheit.
-Inside each function, use the respective global conversion factor to perform the conversion.
-User Interaction:
-
-Prompt the user to enter a temperature and specify whether it’s in Celsius or Fahrenheit.
-Based on the user’s input, call the appropriate conversion function and display the converted temperature.
-If gthe user entered a wrong input,raise an error “Invalid temperature. Please enter a numeric value.”
-"""
-
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 

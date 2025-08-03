21e8ea7d18fe16d77c44c957d8b238deb7143c4f
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-08-03 18:06:11 +0100

    bank account done

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount():
+    def __init__(self, account_balance, initial_balance=0):
+        self.account_balance = account_balance
+        self.initial_balance = initial_balance
+    
+    def deposit(self, amount):
+        self.account_balance += amount
+        return None
+    
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        self.account_balance -= amount
+        return True
+    
+    def display_balance(self):
+        print(f"Current balance is ${self.account_balance}")
+        return None
+    
+
+    
+
+    

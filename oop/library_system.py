be84b3c0d66f219505e24375f4e019702fb558de
Ayomide Johnson Salami <auomidejohnson@gmail.com>
2025-08-08 08:33:51 +0100

    added string representation

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,18 +4,24 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.__class__.__name__}: {self.title}"
+        return f"{self.__class__.__name__}: {self.title} by {self.author}"
           
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size =  file_size
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
@@ -28,4 +34,4 @@ class Library:
             raise TypeError("Only instances of Book or its subclasses can be added.")
         
     def list_books(self):
-        [print(f"{book.__class__.__name__}: {book.title}") for book in self.books]
+        [print(book) for book in self.books]
